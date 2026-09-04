8b90679d204c92d84b0ee6d045d51bc90e82e8c7
Abhi71-star <203789421+Abhi71-star@users.noreply.github.com>
2026-09-04 17:18:57 +0530

    lb

--- day-3-lb-asg/main.tf
+++ day-3-lb-asg/main.tf
@@ -58,7 +58,7 @@ resource "aws_lb_listener" "lb_listener" {
     port = 80
     protocol = "HTTP"
     default_action {
-        type = "forword"
+        type = "forward"
         target_group_arn = aws_lb_target_group.tg.arn 
     }
 }
