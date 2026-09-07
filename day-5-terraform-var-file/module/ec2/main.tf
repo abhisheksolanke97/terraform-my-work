65dc00175b3343e569c94f3fd713bdb124383b90
Abhi71-star <203789421+Abhi71-star@users.noreply.github.com>
2026-09-08 00:00:39 +0530

    day-5-module

--- day-5-terraform-var-file/module/ec2/main.tf
+++ day-5-terraform-var-file/module/ec2/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "public_instance" {
     instance_type = var.instance_type
     key_name = var.key_name
     subnet_id = var.public_subnet_id 
-    vpc_security_group_ids = var.sg_id 
+    vpc_security_group_ids = [var.sg_id]
     associate_public_ip_address = true 
     user_data = file("/root/terraform-my-work/day-5-terraform-var-file/user_data.sh")
     tags = {
@@ -16,7 +16,7 @@ resource "aws_instance" "private_instance" {
     instance_type = var.instance_type
     key_name = var.key_name
     subnet_id = var.private_subnet_id
-    vpc_security_group_ids = var.sg_id
+    vpc_security_group_ids = [var.sg_id]
     associate_public_ip_address = false
     user_data = file("/root/terraform-my-work/day-5-terraform-var-file/user_data.sh")
     tags = {
--- day-5-terraform-var-file/terraform.tfvars
+++ day-5-terraform-var-file/terraform.tfvars
@@ -3,7 +3,7 @@ private_subnet_cidr = "10.0.0.0/21"
 public_subnet_cidr = "10.0.8.0/21"
 public_az = "us-west-2a"
 private_az = "us-west-2b"
-sg-name = "my_sg"
+sg_name = "my_sg"
 http_port = 80
 ssh_port = 22
 
