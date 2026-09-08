8226d8e988276e41b2f5e5efd004e2a80aaa87ad
Abhi71-star <203789421+Abhi71-star@users.noreply.github.com>
2026-09-08 14:20:35 +0530

    day-6-state-management

---
+++ day-6-state-management/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "public_instance" {
+    ami = "ami-0bea529386a62a2ad"
+    instance_type = "t3.micro"
+    key_name = "key0"
+    count = 2
+    vpc_security_group_ids = ["sg-08675da805eab2644"]
+    tags = {
+        Name = "public_instance"
+    }
+}
