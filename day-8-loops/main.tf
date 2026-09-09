7198c6374332795b6e871dfd76b621c216f2bd32
Abhi71-star <203789421+Abhi71-star@users.noreply.github.com>
2026-09-09 14:37:22 +0530

    day-8

---
+++ day-8-loops/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instane" "ec2" {
+    for_each = tomap({
+        server-1 = "t3.micro"
+        server-2 = "t3.small"
+        server-3 = "c7i-flex.large"
+    })
+    ami = "ami-0bea529386a62a2ad"
+    instance_type = echo.value 
+    key_name = "key0" 
+    tags = {
+        Name = echo.key 
+    }
+}
