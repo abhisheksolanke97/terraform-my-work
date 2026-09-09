49669d7b617309a95e9d1428de4c0f3ef8caa5bd
Abhi71-star <203789421+Abhi71-star@users.noreply.github.com>
2026-09-09 14:16:01 +0530

    day-7

--- day-7-workspace/main.tf
+++ day-7-workspace/main.tf
@@ -3,9 +3,9 @@ resource "random_id" "random_id" {
 }
 
 resource "aws_s3_bucket" "s3" {
-    bucket = "demo_bucket-${terraform.workspace}-${random_id.random_id.hex}"
+    bucket = "demo-bucket-${terraform.workspace}-${random_id.random_id.hex}"
 
     tags = {
-        Name = "demo_bucket-${terraform.workspace}"
+        Name = "demo-bucket-${terraform.workspace}"
     }
-}
+} 
